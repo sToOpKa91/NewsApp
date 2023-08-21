c9889a8dabb4d4f93eefa37d4e716a0b4268819b
stoopka <stepanbulanov1991@gmail.com>
2023-08-21 15:03:51 +0300

    git branch -M main

---
+++ app/src/main/java/com/sto_opka91/newsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sto_opka91.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
